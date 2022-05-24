dfdaccb825e33e45cd07e5dd41f77bca09161d7e
vagrant <vagrant@vagrant.vm>
2022-05-24 19:11:11 +0000

    task 2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,14 @@
 section .text
         global _start
-_strat:
+_start:
         mov edx, len
         mov ecx, msg
         mov ebx, 1
         mov eax, 4             ;system call (sys_write)
         int 0x80               ;to call kernel
-
+               
+        mov eax, 1             ;system call (sys_exit)
+        int 0x080
 section .data
-     msg db "Hello, Holberton",0xa ;also 10
+     msg db "Hello, Holberton",0xa ; also 10
      len equ $ -msg
